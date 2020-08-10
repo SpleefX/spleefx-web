87fae8f0ad1e9e00ab014a97457c5a828c8e7cf3
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2020-08-10 13:56:14 +0300

    Dark magic thank you Spring

--- src/main/kotlin/net/spleefx/api/SpleefXAPI.kt
+++ src/main/kotlin/net/spleefx/api/SpleefXAPI.kt
@@ -3,8 +3,14 @@ package net.spleefx.api
 import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.apache.catalina.Context
+import org.apache.catalina.connector.Connector
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableAsync
 import java.io.File
@@ -23,6 +29,32 @@ class SpleefXAPI {
      */
     @Bean
     fun taskExecutor(): ExecutorService = EXECUTOR
+
+    @Bean
+    fun servletContainer(): ServletWebServerFactory {
+        val tomcat: TomcatServletWebServerFactory = object : TomcatServletWebServerFactory() {
+            override fun postProcessContext(context: Context) {
+                val securityConstraint = SecurityConstraint()
+                securityConstraint.userConstraint = "CONFIDENTIAL"
+                val collection = SecurityCollection()
+                collection.addPattern("/*")
+                securityConstraint.addCollection(collection)
+                context.addConstraint(securityConstraint)
+            }
+        }
+        tomcat.addAdditionalTomcatConnectors(httpConnector)
+        return tomcat
+    }
+
+    private val httpConnector: Connector
+        get() {
+            val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+            connector.scheme = "http"
+            connector.port = 8080
+            connector.secure = false
+            connector.redirectPort = 443
+            return connector
+        }
 
     companion object {
 
